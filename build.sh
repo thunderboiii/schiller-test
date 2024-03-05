6a0da18bcf95de213bced6a71e3cbe35a2a4ebf3
thunderboiii <homepage@schiller.ms.de>
2024-03-05 14:05:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-# Run the build command
-npm run build
+# FTP credentials
+FTP_HOST="217.21.77.165"
+FTP_USER="u261657911"
+FTP_PASS="ftp:maple-1NK"
+
+# Directory containing files to upload
+LOCAL_DIR="./public"
+
+# Remote directory on the server
+REMOTE_DIR="/public_html/schiller"
+
+# Connect to FTP server and upload files
+lftp -u "${FTP_USER},${FTP_PASS}" "${FTP_HOST}" <<EOF
+mirror -R "${LOCAL_DIR}" "${REMOTE_DIR}"
+exit
+EOF
